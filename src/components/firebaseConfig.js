1e68bb886ea7ceec7056efdd7477a8f523043784
dedZombie <hemicar92@gmail.com>
2017-12-18 15:34:02 +0100

    app init

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAa_XIf3O8MeaB8NXh_6OdVRuhNZabHlME",
+    authDomain: "vuefs-pd.firebaseapp.com",
+    databaseURL: "https://vuefs-pd.firebaseio.com",
+    projectId: "vuefs-pd",
+    storageBucket: "vuefs-pd.appspot.com",
+    messagingSenderId: "216261529515"
+}
